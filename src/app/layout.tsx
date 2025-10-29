d315aa699c48379d56a2568826aa522e6f3ffaa1
Daniel <komarov@arnca.pro>
2025-10-29 14:11:50 +0500

    added header to layout for reusage on every page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/shared/header/header";
 
 const nunitoSans = Nunito_Sans({
   variable: "--font-nunito-sans",
@@ -25,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${nunitoSans.variable} ${nunito.variable} antialiased`}>
-        <main className="min-h-screen">{children}</main>
+        <main className="min-h-screen">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -1,12 +1,39 @@
+import { Container } from "@/components/shared/container/container";
 import { cn } from "@/lib/utils";
 import { FC, FunctionComponent } from "react";
+import Image from "next/image";
 
 type Props = {
   externalClass?: string;
 };
 
-export const header: FunctionComponent<Props> = (props) => {
+const LOGO_WIDTH = 35;
+const LOGO_HEIGHT = 35;
+
+export const Header: FunctionComponent<Props> = (props) => {
   const { externalClass } = props;
 
-  return <header className={cn("border border-b ", externalClass)}>header</header >;
+  return (
+    <header className={cn("border-b text-(--text-grey)", externalClass)}>
+      <Container externalClass="flex items-center justify-between py-8">
+        <div>
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={LOGO_WIDTH}
+            height={LOGO_HEIGHT}
+          />
+
+          <div>
+            <h1 className="text-2xl text-black uppercase font-black">
+              Next Pizza
+            </h1>
+            <p className="text-sm text-gray-400 leading-3">
+              вкусней уже некуда
+            </p>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
